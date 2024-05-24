f967c00a5873fc7cc6db7f92d00fae3d7763adb5
jismy12 <jismy.woodenclouds@gmail.com>
2024-05-24 17:51:05 +0530

    chapters

--- src/app/chapters/page.js
+++ src/app/chapters/page.js
@@ -1,12 +1,14 @@
 "use client";
+import Chapters from "@/components/maincomponents/chapterssection/Chapters";
 import HomeComponents from "@/components/maincomponents/homecomponents/HomeComponents";
+import PlayerComponents from "@/components/maincomponents/playercomponents/PlayerComponents";
 import PlayerExpanded from "@/components/maincomponents/playerexpanded/PlayerExpanded";
 
 export default function Home({ Component, pageProps }) {
   return (
     <>
       <HomeComponents />
-      <PlayerExpanded />
+      <Chapters />
     </>
   );
 }
